3e6c6de3703910297f5e5ea47d59f58e73bd9022
Circé <circegrand@gmail.com>
2017-11-20 21:33:57 +0100

    Add data json

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -2,17 +2,12 @@
 //     $('.loader').css('display', 'none')
 // }
 
+console.log(sumup);
+
 function setup() {
  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-
- //OBJECT FOLLOW MOUSE
- clear()
- fill(255,0,0, 50)
- noStroke()
- ellipse(mouseX, mouseY, 20)
-
 
 }
