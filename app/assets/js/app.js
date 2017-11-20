0fcb4cd82dcbbc47b7a60f2dc7d89d443b486026
Circé <circegrand@gmail.com>
2017-11-20 18:55:35 +0100

    Add p5 processing + scss files

---
+++ app/assets/js/app.js
@@ -0,0 +1,18 @@
+// window.onload = () => {
+//     $('.loader').css('display', 'none')
+// }
+
+function setup() {
+ createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+ //OBJECT FOLLOW MOUSE
+ clear()
+ fill(255,0,0, 50)
+ noStroke()
+ ellipse(mouseX, mouseY, 20)
+
+
+}
