cf0d9c4f0cd8379c301787adbcd8b6bb3cf294be
Circé <circegrand@gmail.com>
2017-11-21 15:08:57 +0100

    Add scss files for all topics, restructure

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,15 +1,43 @@
+/* LOADER DISPLAY */
 // window.onload = () => {
-//     $('.loader').css('display', 'none')
+//     $('.loading').css('display', 'none')
 // }
 
-console.log(summary);
-
-$('#test').text(summary.questions[0].title)
-
-function setup() {
- createCanvas(windowWidth, windowHeight);
+/* LOAD JSON FILE */
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
 }
 
-function draw() {
+ajaxGet('./assets/data/data.json', (data) => {
+  data = JSON.parse(data)
+  drawTopic1(data)
+})
 
+  /* TOPIC 1: DRAW CIRCLES */
+var canvas = document.getElementById('topic1')
+var ctx = canvas.getContext('2d')
+
+function drawTopic1(data) {
+  let responsesLength = data.questions[0].responses.length;
+    for (var j = 0; j < responsesLength; j++) {
+      var radius = data.questions[0].responses[j].people
+      ctx.beginPath();
+      ctx.arc(100*(j+1), 100*(j+1), radius * 4, 0, 2 * Math.PI, false);
+      ctx.fillStyle = 'green';
+      ctx.fill();
+      ctx.strokeStyle = '#003300';
+      ctx.stroke();
+    }
 }
