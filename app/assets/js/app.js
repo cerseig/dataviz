e273d09534afcf0a2c7cb263908703739c19069e
Circé <circegrand@gmail.com>
2017-11-21 21:23:38 +0100

    Add muli font + loader + homepage

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,7 +1,45 @@
-/* LOADER DISPLAY */
-// window.onload = () => {
-//     $('.loading').css('display', 'none')
-// }
+/**
+ * Loader de page
+ */
+$.pageLoader = function() {
+
+  var $loader = $('.loading');
+  var $chargementInfos = $('#count');
+  var dureeMs = 1000;
+
+  function animateStep(now, fx) {
+    $chargementInfos.text(parseInt(now)+'%');
+  }
+
+  function chargementEnCours() {
+    var pourcentage = 0;
+    if($elements.length) {
+      pourcentage = parseInt((elementsCharges / $elements.length) * 100);
+    }
+  }
+
+  function chargementTermine() {
+    var pourcentage = 100;
+
+    // Affichage du pourcentage
+    $chargementInfos
+      .stop() // stop les anciennes animations
+      .animate({pourcentage:pourcentage}, {
+        duration: (dureeMs / 2),
+        step: animateStep
+      })
+      // Disparition du chargement et affichage de la page
+      .css({opacity: 1})
+      .animate({opacity: 0}, function() {
+        // La page est prete
+        $loader.css({display:'none'});
+      });
+  }
+
+  $(window).load(function() {
+    chargementTermine();
+  });
+};
 
 /* LOAD JSON FILE */
 function ajaxGet(url, callback) {
@@ -23,6 +61,81 @@ function ajaxGet(url, callback) {
 ajaxGet('./assets/data/data.json', (data) => {
   data = JSON.parse(data)
   drawTopic1(data)
+})
+
+/* CTA START */
+$('#start').on('click', () => {
+  setTimeout(function() {
+    $('.circle7').addClass('animate')
+    setTimeout(function() {
+      $('.circle7').fadeOut('slow')
+    }, 500)
+  }, 0)
+  setTimeout(function() {
+    $('.circle9').addClass('animate')
+    setTimeout(function() {
+      $('.circle9').fadeOut('slow')
+    }, 500)
+  }, 200)
+  setTimeout(function() {
+    $('.circle11').addClass('animate')
+    setTimeout(function() {
+      $('.circle11').fadeOut('slow')
+    }, 500)
+  }, 400)
+  setTimeout(function() {
+    $('.circle6').addClass('animate')
+    setTimeout(function() {
+      $('.circle6').fadeOut('slow')
+    }, 500)
+  }, 600)
+  setTimeout(function() {
+    $('.circle5').addClass('animate')
+    setTimeout(function() {
+      $('.circle5').fadeOut('slow')
+    }, 500)
+  }, 800)
+  setTimeout(function() {
+    $('.circle4').addClass('animate')
+    setTimeout(function() {
+      $('.circle4').fadeOut('slow')
+    }, 500)
+  }, 1000)
+  setTimeout(function() {
+    $('.circle1').addClass('animate')
+    setTimeout(function() {
+      $('.circle1').fadeOut('slow')
+    }, 500)
+  }, 1200)
+  setTimeout(function() {
+    $('.circle8').addClass('animate')
+    setTimeout(function() {
+      $('.circle8').fadeOut('slow')
+    }, 500)
+  }, 1400)
+  setTimeout(function() {
+    $('.circle2').addClass('animate')
+    setTimeout(function() {
+      $('.circle2').fadeOut('slow')
+    }, 500)
+  }, 1600)
+  setTimeout(function() {
+    $('.circle10').addClass('animate')
+    setTimeout(function() {
+      $('.circle10').fadeOut('slow')
+    }, 500)
+  }, 1800)
+  setTimeout(function() {
+    $('.circle3').addClass('animate')
+    setTimeout(function() {
+      $('.circle3').fadeOut('slow')
+      $('.content-home').fadeOut('slow')
+      setTimeout(function() {
+        $('.home').fadeOut('slow')
+        $('.topic1').fadeIn('slow')
+      }, 1000)
+    }, 500)
+  }, 2000)
 })
 
   /* TOPIC 1: DRAW CIRCLES */
